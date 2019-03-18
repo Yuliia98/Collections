257ad443436ac4ef6d8dd95126fe0cd7950f4005
Serhii-Yasenev <serhii_yasenev@epam.com>
2019-03-18 17:24:09 +0200

    upd

--- HW3(Collections)/Optional(Dictionary)/Actions.cs
+++ HW3(Collections)/Optional(Dictionary)/Actions.cs
@@ -7,41 +7,36 @@ namespace Optional_Dictionary_
     {
         public static void CodeGroup(Dictionary<string, List<Student>> dict)
         {
-            var input = "";
-            var answer = "";
-            var data = "";
-
-            var listOfGroup = new List<Student>();
             Console.WriteLine("Enter code group. Or enter 'exit' to exit");
-            input = Console.ReadLine();
+            var input = Console.ReadLine();
             while (input != "exit")
             {
-
-                if (dict.ContainsKey(input) == true)
+                if (dict.ContainsKey(input ?? throw new InvalidOperationException()))
                 {
-                    dict.TryGetValue(input, out listOfGroup);
-                    foreach (var value in listOfGroup)
-                    {
-                        Console.WriteLine($"{value.Surname}:{value.Date} ");
-                    }
+                    dict.TryGetValue(input, out var listOfGroup);
+                    if (listOfGroup != null)
+                        foreach (var value in listOfGroup)
+                        {
+                            Console.WriteLine($"{value.Surname}:{value.Date} ");
+                        }
                 }
                 else
                 {
                     Console.WriteLine("There is no group with this code. Do you want added it (y/n)");
-                    answer = Console.ReadLine();
+                    var answer = Console.ReadLine();
                     dict[input] = new List<Student>();
                     if (answer == "y")
                     {
                         while (true)
                         {
                             Console.WriteLine("Enter enter students in format: Surname1:Birthday1, Surname2:Bithday2");
-                            data = Console.ReadLine();
+                            var data = Console.ReadLine();
                             try
                             {
-                                string[] students = data.Split(',');
-                                for (int i = 0; i < students.Length; i++)
+                                var students = data?.Split(',');
+                                foreach (var student in students)
                                 {
-                                    string[] dataOfStud = students[i].Split(':');
+                                    var dataOfStud = student.Split(':');
                                     dict[input].Add(new Student(dataOfStud[0], int.Parse(dataOfStud[1])));
                                 }
                                 break;
@@ -55,8 +50,7 @@ namespace Optional_Dictionary_
                 }
                 Console.WriteLine("Enter code group. Or enter 'exit' to exit");
                 input = Console.ReadLine();
-
-            };
+            }
         }
     }
 }
--- HW3(Collections)/Optional(Dictionary)/Program.cs
+++ HW3(Collections)/Optional(Dictionary)/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Optional_Dictionary_
 {
@@ -6,11 +7,17 @@ namespace Optional_Dictionary_
     {
         static void Main(string[] args)
         {
-            var group = new Dictionary<string, List<Student>>();
-            group.Add("TV01", new List<Student> { new Student("Shishman", 1998), new Student("Dvornik", 1997) });
-            group.Add("TV02", new List<Student> { new Student("Goroshko", 1995), new Student("Zubrich", 1998), new Student("Soloma", 1996) });
-            group.Add("TV03", new List<Student> { new Student("Novichenko", 1994), new Student("Zhmurckevich", 1997), new Student("Aslamova", 1993) });
-            Actions.CodeGroup(group);
+            var groups = new Dictionary<string, List<Student>>();
+            groups.Add("TV01", new List<Student> { new Student("Shishman", 1998), new Student("Dvornik", 1997) });
+            groups.Add("TV02", new List<Student> { new Student("Goroshko", 1995), new Student("Zubrich", 1998), new Student("Soloma", 1996) });
+            groups.Add("TV03", new List<Student> { new Student("Novichenko", 1994), new Student("Zhmurckevich", 1997), new Student("Aslamova", 1993) });
+            Actions.CodeGroup(groups);
+            Console.WriteLine("Existing groups are:");
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group.Key);
+            }
+            Console.ReadKey();
         }
     }
 }
